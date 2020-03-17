477d72f6d88486a00b000da0f2d3a0a0e64a067c
sandeep poomangalath <sandeep@hooq.tv>
2020-03-17 23:29:53 +0800

    fixed issue with production

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV == 'production') {
 
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(__dirname, 'client', 'build', 'index.html');
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
