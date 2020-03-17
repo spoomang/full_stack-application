c2c1c08ea88dffa9250adedf54d980f902ebd11b
sandeep poomangalath <sandeep@hooq.tv>
2020-03-17 23:03:28 +0800

    changes for production

--- index.js
+++ index.js
@@ -23,5 +23,14 @@ app.use(passport.session());
 
 authroutes(app);
 
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname, 'client', 'build', 'index.html');
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
